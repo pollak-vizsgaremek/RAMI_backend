20b20462d8229f1c18d8d1a43f72930283714b72
schwarczotto7 <schwarcz.yoda@gmail.com>
2025-09-24 09:22:28 +0200

    prisma, cors

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-import e from 'express';
-import cors from 'cross';
+import e from "express";
+import cors from "cors";
 
 const app = e();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
-}
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 
 
 
 
 app.listen(3300, () => {
-    console.log("App startedc at http://localhost:3300")
+    console.log("App started at http://localhost:3300")
 })
